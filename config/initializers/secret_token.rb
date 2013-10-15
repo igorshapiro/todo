d7ff45e99005dd02167f9ee4daa9ce849e51986f
Igor Shapiro <igor@licensario.com>
2013-10-15 17:46:33 +0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cellrox::Application.config.secret_token = 'f560dfe55b0888de350ba934b9431ee9a6529e3ede8c057e11f3588d5820cb263026eee4988d2456cea22472f263d5b97e343f6ff64359f4f23d46fadd6329f8'
